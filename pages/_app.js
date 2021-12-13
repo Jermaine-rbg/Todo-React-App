c5b57c0afb18776999c863cb61faee1af0acecca
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2021-12-12 21:46:17 -0500

    app.js config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import customTheme from "../lib/theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={customTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
